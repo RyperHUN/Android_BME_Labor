e889891c8db63f7f7ee0433d07dad618b5bcff09
RyperHUN <akormanyzsolt@gmail.com>
2016-09-22 11:43:42 +0200

    Added delete functionality

--- Project/MyApplication/app/src/main/java/hu/aut/bme/androidwallet/MainActivity.java
+++ Project/MyApplication/app/src/main/java/hu/aut/bme/androidwallet/MainActivity.java
@@ -88,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_delete_all) {
+            listOfRows.removeAllViews();
             return true;
         }
 
