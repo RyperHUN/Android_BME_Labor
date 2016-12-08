ff9db48c8d1e2efc69282a09f3ef3b19e3437d11
RyperHUN <akormanyzsolt@gmail.com>
2016-12-09 00:23:12 +0100

    Added missing files

---
+++ HomeWork/app/src/main/java/ryper/homeworkimprovement/Main/Pager/CategoryPagerManager.java
@@ -0,0 +1,86 @@
+package ryper.homeworkimprovement.Main.Pager;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+import ryper.homeworkimprovement.DB.Categories;
+
+/**
+ * Created by Ryper on 2016. 12. 08..
+ */
+public class CategoryPagerManager extends FragmentPagerAdapter {
+    private Context context;
+    ArrayList<TodoCategoryFragment> fragments = null;
+
+    public CategoryPagerManager(FragmentManager fm, Context context) {
+        super(fm);
+        fragments = new ArrayList<>();
+        this.context = context;
+    }
+
+    // Ez a függvény csak egyszer hívódik meg, nem fog feleslegesen sok
+// ugyanolyan Fragment-et létrehozni!
+    @Override
+    public Fragment getItem(int position) {
+        Fragment ret = null;
+        TodoCategoryFragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new TodoCategoryFragment().Init(context, Categories.AKey);
+                ret = fragment;
+                break;
+            case 1:
+                fragment = new TodoCategoryFragment().Init(context, Categories.BKey);
+                ret = fragment;
+                break;
+            case 2:
+                fragment = new TodoCategoryFragment().Init(context, Categories.CKey);
+                ret = fragment;
+                break;
+            case 3:
+                fragment = new TodoCategoryFragment().Init(context, Categories.DKey);
+                ret = fragment;
+                break;
+        }
+        if (fragment != null)
+            fragments.add(fragment);
+        return ret;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title;
+        switch (position) {
+            case 0:
+                title = Categories.getCategory(Categories.AKey);
+                break;
+            case 1:
+                title = Categories.getCategory(Categories.BKey);
+                break;
+            case 2:
+                title = Categories.getCategory(Categories.CKey);
+                break;
+            case 3:
+                title = Categories.getCategory(Categories.DKey);
+                break;
+            default:
+                title = "";
+        }
+        return title;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    public void refresh() {
+        for (TodoCategoryFragment fragment : fragments)
+            fragment.refreshAdapterDataFromDB();
+    }
+}
+
---
+++ HomeWork/app/src/main/java/ryper/homeworkimprovement/Main/Pager/PagerActivity.java
@@ -0,0 +1,97 @@
+package ryper.homeworkimprovement.Main.Pager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import ryper.homeworkimprovement.DB.Categories;
+import ryper.homeworkimprovement.DB.TodoProgress;
+import ryper.homeworkimprovement.Dialog.AddTodoDialogFragment;
+import ryper.homeworkimprovement.Dialog.AddTodoDialogListener;
+import ryper.homeworkimprovement.History.HistoryActivity;
+import ryper.homeworkimprovement.Main.Pager.CategoryPagerManager;
+import ryper.homeworkimprovement.R;
+
+public class PagerActivity extends AppCompatActivity implements AddTodoDialogListener {
+    CategoryPagerManager categoryPagerManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Categories.InitCategories(this);
+
+        DBFillwithTestData();
+        setContentView(R.layout.activity_pager);
+        ViewPager mainViewPager = (ViewPager) findViewById(R.id.mainViewPager);
+        categoryPagerManager = new CategoryPagerManager(getSupportFragmentManager(), this);
+        mainViewPager.setAdapter(categoryPagerManager);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setFloatingButton ();
+
+    }
+
+    private void setFloatingButton() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddTodoDialogFragment().show( getSupportFragmentManager(), AddTodoDialogFragment.TAG);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_history) {
+            Intent intent = new Intent(this, HistoryActivity.class);
+            startActivity (intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void DBFillwithTestData() {
+        try {
+            TodoProgress.deleteAll(TodoProgress.class);
+
+            InitProgressDatabase ();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void InitProgressDatabase() throws Exception {
+        TodoProgress todoProgress = new TodoProgress("Elmenni tejert", Categories.AKey);
+        todoProgress.save();
+        TodoProgress todoProgress2 = new TodoProgress("Elmenni Virsliert", Categories.BKey);
+        todoProgress2.save ();
+        TodoProgress todoProgress3 = new TodoProgress("Zuckerberg Ebed", Categories.CKey);
+        todoProgress3.save();
+        TodoProgress todoProgress4 = new TodoProgress("Pizzazas", Categories.DKey);
+        todoProgress4.save();
+    }
+
+    @Override
+    public void onTodoAdded(TodoProgress todoProg) {
+        todoProg.save();
+        categoryPagerManager.refresh();
+    }
+
+}
